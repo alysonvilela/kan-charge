184d98ee97a3cc5f36760f88374fc7fc2d4cc6c5
Alyson Vilela do Nascimento <alyson.nascimento.cit@naturapay.net>
2024-06-16 19:25:07 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const processEnvValues = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
