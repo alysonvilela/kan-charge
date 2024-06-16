9944b9c2e6da880834185729747338aaaf6fef9a
Alyson Vilela do Nascimento <alyson.nascimento.cit@naturapay.net>
2024-06-16 11:25:20 -0300

    fix: envs

--- src/screens/clients-screen.tsx
+++ src/screens/clients-screen.tsx
@@ -1,8 +1,11 @@
 export const ClientsScreen = () => {
 
     return (
-        <section className="pt-8">
-            <h1>Upload</h1>
+        <section className="pt-16 w-full">
+            <div className="max-w-md pb-16">
+                <h1 className="text-slate-800 text-xl font-extrabold sm:text-2xl">Cobranças efetuadas</h1>
+                <p className="text-slate-600 mt-2">Listagem de todas as cobrancas efetuadas.</p>
+            </div>
         </section>
     )
 }
